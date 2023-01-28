c82d8f1a8513b950b956170f0e2f8c89d389c2a6
ravandra <123793676+ravandra@users.noreply.github.com>
2023-01-28 10:44:46 +0530

    Add files via upload

---
+++ Demo/Connections.java
@@ -0,0 +1,23 @@
+package Demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connections 
+{
+	public static void main(String[] args) 
+	{
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","system","tiger");
+			System.out.println("Connection Established Successfully");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
